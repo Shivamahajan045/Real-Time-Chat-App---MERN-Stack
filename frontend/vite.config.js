7ce3ab26ca33372395bebd085734ee4ad02d35a6
Shivamahajan045 <111225580+Shivamahajan045@users.noreply.github.com>
2026-04-03 20:20:14 +0530

    fix: add alias and dirname support

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,45 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: "./",
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"), // 🔥 FIX
+//     },
+//   },
+//   optimizeDeps: {
+//     include: ["lottie-react"],
+//   },
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "http://127.0.0.1:5000",
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   base: "./",
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   optimizeDeps: {
     include: ["lottie-react"],
   },
