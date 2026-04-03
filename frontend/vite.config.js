61f6abf81e7e3d7de30e12de6823223d4e27a3fa
Shivamahajan045 <111225580+Shivamahajan045@users.noreply.github.com>
2026-04-03 20:15:33 +0530

    fix: move vite plugin to dependencies

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   optimizeDeps: {
     include: ["lottie-react"],
   },
